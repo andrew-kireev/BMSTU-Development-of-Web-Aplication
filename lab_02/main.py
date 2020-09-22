eab1c9c8661208f43df175de1891a866b16e7ae1
Andrew <a-kireev1989@mail.ru>
2020-09-22 18:14:25 +0300

    add __name__

--- lab_02/main.py
+++ lab_02/main.py
@@ -3,14 +3,17 @@ from lab_python_oop import circle as cir
 from lab_python_oop import Square as sq
 import requests
 
-rec = rec.Rectangle(3, 2, 'blue')
-print(rec)
-circle = cir.Circle(5, 'green')
-print(circle)
-square = sq.Square(4, 'red')
-print(square)
 
-requst = requests.get('https://www.bmstu.ru')
-print('Result of requsts: ' + str(requst.status_code))
 
+if __name__ == "__main__":
+    rec = rec.Rectangle(3, 2, 'blue')
+    print(rec)
+    circle = cir.Circle(5, 'green')
+    print(circle)
+    square = sq.Square(4, 'red')
+    print(square)
+
+    request = requests.get('https://www.bmstu.ru')
+    print('Result of request: ' + str(request.status_code))
+    print(request.headers)
 
